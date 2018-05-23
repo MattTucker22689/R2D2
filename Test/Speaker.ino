57ee1a97937d9b5e5b04a49bb01d29054fc4059f
Matt Tucker <mathew.tucker.1@gmail.com>
2018-05-22 23:07:04 -0400

    Update Speaker.ino

--- Test/Speaker.ino
+++ Test/Speaker.ino
@@ -1,8 +1,9 @@
-/* Author:
-   Matt Tucker
+/* 
+   Author:
+            Matt Tucker
    Description:
-   This is to run thru a list of 61 songs(2-62)
-   and play them as the user sends "a".
+            This is to run thru a list of 61 songs(2-62)
+            and play them as the user sends "a".
 */
 
 #include <SD.h>
@@ -17,15 +18,16 @@ String lab;
 
 void setup()
 {
-
   Serial.begin(9600);
   tmrpcm.speakerPin = 11;
   tmrpcm.setVolume(5);
+  //Checks if MicroSD Reader has anything to send
   if (!SD.begin(SDPIN))
   {
     Serial.println("Initialization Failed!");
     return;
   }
+  //Let's you "hear" that the program is working
   tmrpcm.play("60.wav");
   Serial.println("Send 'a' to play: ");
 }
@@ -40,6 +42,9 @@ void loop()
       lab = String(i) + ".wav";
       Serial.println("playing: " + lab );
       tmrpcm.play(lab.c_str());
+      //The loop below is the LONG way around moving through our files.
+      //For shortest method-> lab = String(i) + ".wav" 
+      //Should read-> lab = String((i%61)+2) + ".wav"
       if (i == 62)
         i = 1;
       i++;
